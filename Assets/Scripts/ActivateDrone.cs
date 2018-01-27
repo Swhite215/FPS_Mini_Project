65bc0b3589ef458d514ab521ccb1caa8137d4c47
Spencer White <Spencer.White@carat.com>
2018-01-27 14:20:53 -0500

    Added drone trigger structure

---
+++ Assets/Scripts/ActivateDrone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateDrone : MonoBehaviour {
+
+	public GameObject drone;
+	private int droneCount = 0;
+	public int droneMax;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player" && droneCount == 0) {
+			drone.SetActive (true);
+			droneCount++;
+		}
+	}
+}
